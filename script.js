1bfae6e3c16c4636881284581ee9782fd1d20ba9
K4Tonn <162270955+K4Tonn@users.noreply.github.com>
2025-01-11 21:41:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const target = document.querySelector(link.getAttribute("href"));
+            target.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
